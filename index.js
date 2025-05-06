be844d65038e8a5f977e2e7243b53fd215d1127f
Jan Kaul <jankaul@mailbox.org>
2025-05-06 09:03:44 +0200

    add default export

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-export * from "./Flight";
+import * as default_1 from "./Flight";
+export default default_1;
 import * as sql_1 from "./FlightSql";
 export { sql_1 as sql };
 export { Any } from "./google/protobuf/any";
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export * from "./Flight"
+export * as default from "./Flight"
 export * as sql from "./FlightSql"
 export { Any } from "./google/protobuf/any";

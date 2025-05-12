4d637123f62974f0f7215b14c0813d7784a6d169
Jan Kaul <jankaul@mailbox.org>
2025-05-12 14:48:49 +0200

    fix ipc message

--- src/index.ts
+++ src/index.ts
@@ -77,17 +77,29 @@ async function collectResults(stream: AsyncIterable<flight.Result>): Promise<Uin
 function flightToIPCStream(stream: AsyncIterable<flight.FlightData>): AsyncIterable<Uint8Array> {
   return async function* () {
     for await (const value of stream) {
-      const headerLength = value.dataHeader.length;
-
-      let buf = new ArrayBuffer(4);
-      const view = new DataView(buf);
-      view.setInt32(0, headerLength, true);
-
-      const message = new Uint8Array(headerLength + value.dataBody.length + 4);
-      message.set(new Uint8Array(buf), 0)
-      message.set(value.dataHeader, 4);
-      message.set(value.dataBody, headerLength + 4);
-      yield message;
+      yield flightToIPCMessage(value);
     }
   }()
 }
+
+function flightToIPCMessage(value: flight.FlightData) {
+    const headerLength = value.dataHeader.length;
+    const padBytes = (8 - (headerLength & 7)) & 7;
+
+    const message = new Uint8Array(8 + headerLength + padBytes + value.dataBody.length);
+
+    // Continuation header
+    message[0] = 0xff;
+    message[1] = 0xff;
+    message[2] = 0xff;
+    message[3] = 0xff;
+
+    new DataView(message.buffer, 4, 4).setInt32(0, headerLength, true);
+
+    message.set(value.dataHeader, 8);
+    message.fill(0, 8 + headerLength, 8 + headerLength + padBytes);
+    message.set(value.dataBody, 8 + headerLength + padBytes);
+
+    return message;
+}
+
